713c1090cf6b7ed36ff2bf96e0b4351a433a9150
Ignacio Martinez <nama1510@gmail.com>
2020-05-28 19:43:26 +0200

    [PRG] Pues un server y un cliente
    pues muchas gracias

--- Source/Client/Client.cpp
+++ Source/Client/Client.cpp
@@ -2,8 +2,45 @@
 //
 
 #include <iostream>
-
+#include "Net/factoryenet.h"
+#include "Net/client.h"
+#include "Net/buffer.h"
+namespace
+{
+    const int s_iPort = 65785;
+}
 int main()
 {
+    Net::CFactory* pFactory = new Net::CFactoryEnet();
+    Net::CClient* pClient = pFactory->buildClient();
 
+    pClient->init(1,0,0);
+    std::cout << "write the server IP: ";
+
+    char sMessage[1024];
+    std::cin >> sMessage;
+
+    Net::CConnection* pConnection = pClient->connect(sMessage, s_iPort, 1, 5000u);
+
+    std::cout << "write your message: (\"exit\" to close connection.)";
+
+    do 
+    {
+        std::cin >> sMessage;
+        if (strcmp(sMessage, "exit") != 0) 
+        {
+            Net::CBuffer oData(1024, 256);
+            oData.write(sMessage, strlen(sMessage));
+
+            Net::CPacket oMessagePacket(Net::DATA, oData.getbuffer(), oData.getSize(), pConnection, 0);
+            pClient->sendData(pConnection, oMessagePacket.getData(), oMessagePacket.getDataLength(), 0, true);
+        }
+    } while (strcmp(sMessage, "exit") != 0);
+
+    pClient->disconnect(pConnection);
+    pClient->release();
+
+    delete pConnection;
+    delete pClient;
+    delete pFactory;
 }
--- Source/Server/Server.cpp
+++ Source/Server/Server.cpp
@@ -2,8 +2,61 @@
 //
 
 #include <iostream>
+#include "Net/factoryenet.h"
+#include "Net/server.h"
+#include "Net/buffer.h"
+
+namespace
+{
+    const int s_iPort = 65785;
+}
 
 int main()
 {
-  
+    Net::CFactory* pFactory = new Net::CFactoryEnet();
+    Net::CServer* pServer = pFactory->buildServer();
+
+    pServer->init(s_iPort, 10);
+    std::vector<Net::CPacket*> pPackets;
+    char sMessage[1024];
+    do
+    {
+        pServer->service(pPackets);
+        for (std::vector<Net::CPacket*>::iterator it = pPackets.begin(); it != pPackets.end(); ++it)
+        {
+            Net::CPacket* pPacket = *it;
+            switch (pPacket->getType())
+            {
+                case Net::CONNECTION:
+                {
+                    std::cout << "a new client has been connected\n";
+                }
+                break;
+                case Net::DATA:
+                {
+                    Net::CBuffer oData;
+                    oData.write(pPacket->getData(), pPacket->getDataLength());
+                    oData.reset();
+                    size_t iSize = oData.getSize();
+                    oData.read(sMessage, iSize);
+                    sMessage[iSize] = '\0';
+                    std::cout << sMessage << "\n";
+                }
+                break;
+                case Net::DISCONNECTION:
+                {
+                    std::cout << "a client has been disconnected\n";
+                }
+                break;
+                default:
+                    break;
+            }
+            delete pPacket;
+        }
+        pPackets.clear();
+    } while (strcmp(sMessage, "closeserver") != 0);
+
+    pServer->release();
+    delete pServer;
+    delete pFactory;
 }
